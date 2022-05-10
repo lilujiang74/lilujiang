e08e27df5391587b7eadd0c6988f451bfa81f09a
llj <1754487136@qq.com>
2022-05-10 16:06:21 +0800

    2022.5.10 do a text

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,10 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
     def tearDown(self):
         self.browser.quit()
+    def check_for_row_in_list_tablb(self,row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
     def test_can_start_a_list_and_retrieve_it_later(self):
         #edith has heard about a cool new online to-do app.she goes
         #to check out its homepage
@@ -26,14 +30,28 @@ class NewVisitorTest(unittest.TestCase):
         )
         #she types "Buy peacock feathers" into a text box (Edith's hobby
         #is tying fiy-fishing lures)
+        inputbox.send_keys('Buy peacock feathers')
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
+        self.check_for_row_in_list_tablb('1: Buy peacock feathers')
+        inputbox=self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_tablb('1: Buy peacock feathers')
+        self.check_for_row_in_list_tablb('2: Use peacock feathers to make a fly')
 
-        table=self.browser.find_element_by_id('id_list_table')
-        rows=table.find_elements_by_tag_name('tr')
+        table=self.browser.find_element_by_id("id_list_table")
+        rows=table.find_elements_by_tag_name("tr")
+        self.assertIn('1: Buy peacock feathers',[row.text for row in rows]
+        )
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         self.assertTrue(
-            any(row.text=='1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            any(row.text=="1: Buy peacock feathers" for row in rows),
+            f"New to-do item did not appear in table. Contents were:\n{table.text}"
         )
         #There is still a text box inviting her to add another item. She
         #enters "Use peacock feathers to make a fly"(Edith is very
