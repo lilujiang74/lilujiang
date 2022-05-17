81c44c1f0d286a669571cacbfcb8ba830b11b694
llj <1754487136@qq.com>
2022-05-17 14:34:07 +0800

    Redirect after POST, and show all items in template

--- lists/tests.py
+++ lists/tests.py
@@ -5,7 +5,19 @@ from django.template.loader  import  render_to_string
 from lists.views import home_page
 from lists.models import Item
 
-#class HomePageTest(TestCase):
+class HomePageTest(TestCase):
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(),0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        reponse=self.client.get('/')
+
+        self.assertIn('itemey 1', reponse.content.decode())
+        self.assertIn('itemey 2', reponse.content.decode())
 #    def test_uses_home_template(self):
 #       response=self.client.get('/')
  #      self.assertTemplateUsed(response, 'home.html')
@@ -32,3 +44,23 @@ class ItemModelTest(TestCase):
         second_saved_item=saved_items[1]
         self.assertEqual(first_saved_item.text,'The first(ever) list item')
         self.assertEqual(second_saved_item.text,'Item the second')
+
+    def test_can_save_a_POST_request(self):
+        #response=self.client.post('/',data={'item_text':'A new list item'})
+
+        #self.assertEqual(Item.objects.count(),1)
+        #new_item=Item.objects.first()
+        #self.assertEqual(new_item.text, 'A new list item')
+
+        #self.assertEqual(response.status_code, 302)
+        #self.assertEqual(response['location'], '/')
+        self.client.post('/',data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
