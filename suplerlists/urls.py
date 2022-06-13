f532232ee143d33fd612e7ea2527a35b096a7954
llj <1754487136@qq.com>
2022-06-13 13:21:56 +0800

    new URL + view for adding to existing lists. FT  passes :-)

--- suplerlists/urls.py
+++ suplerlists/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from lists import views
-from django.urls import path
+from django.urls import path, re_path
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('lists/the-only-list-in-the-world/', views.view_list, name='view_list'),
-    path('lists/new',views.new_list,name='new_list')
+    path('lists/new', views.new_list, name='new_list'),
+    re_path(r'^lists/(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^lists/(\d+)/add_item$', views.add_item, name='add_item'),
+   
 ]
